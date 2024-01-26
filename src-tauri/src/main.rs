5c930a68e4b8f3a08056882be9bdbba0535f928b
Ayron Wohletz <ayron.wohletz@gmail.com>
2024-01-25 20:43:28 -0700

    Initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,46 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::Manager;
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+    message: String,
+}
+
+fn main() {
+    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let submenu = Submenu::new("File", Menu::new().add_item(quit));
+    let menu = Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_item(CustomMenuItem::new("hide", "Hide"))
+        .add_submenu(submenu);
+
+    tauri::Builder::default()
+        .setup(|app| {
+            Ok(())
+        })
+        .menu(menu)
+        .on_menu_event(|event| {
+            match event.menu_item_id() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "hide" => {
+                    // event.window().emit("greet", Payload {message: "Hide me".into()}).unwrap();
+                }
+                _ => {}
+            }
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
